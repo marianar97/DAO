4a53a30891aad247604631e8811313362243f96b
marianar97 <marianar97@LAPTOP-J03EB5V8.localdomain>
2019-03-18 01:18:36 -0500

    adding command line interface

--- src/store/Main.java
+++ src/store/Main.java
@@ -11,12 +11,11 @@ import java.text.DateFormat;
 import java.text.ParseException;
 
 import java.sql.SQLException;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+
+    static Scanner sc;
 
     public static void main(String[] args) throws SQLException, DAOException {
 
@@ -52,84 +51,189 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        String tipo = null;
-        String operacion = null;
-        tipo = sc.next();
-        SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy");
 
-
-        switch (tipo){
+        String type = null;
+        type = sc.next();
+        switch (type){
             case "C":
-                firstInstruction("cliente");
-                operacion = sc.next();
-                switch (operacion) {
-                    case "I":
-                        Client clientInsert = getClient();
-                        msq.getClientDao().create(clientInsert);
-
-                        break;
-                    case "A":
-                        List<Client> clients = msq.getClientDao().getAll();
-                        for (Client a : clients) {
-                            System.out.println(a.toString());
-                        }
-                        break;
-                    case "U":
-                        Client clientUpdate = getClient();
-                        System.out.println("Ingrese el id del cliente");
-                        int id = sc.nextInt();
-                        clientUpdate.setClientId(id);
-                        msq.getClientDao().update(clientUpdate);
-                        break;
-                    case "D":
-                        System.out.println("Ingrese el id del cliente que desea eliminar");
-                        Client cdefault = new Client("d", "d", "d", new Date(0, 0, 0), "d");
-                        int idC = sc.nextInt();
-                        cdefault.setClientId(idC);
-                        msq.getClientDao().delete(cdefault);
-                        break;
-                    case "G":
-                        System.out.println("Ingrese el id del cliente que desea listar");
-                        int idG = sc.nextInt();
-                        Client oneClient = msq.getClientDao().get(idG);
-                        System.out.println(oneClient);
-                        break;
-                }
+                client();
+                break;
             case "T":
-                firstInstruction("item");
-                operacion = sc.next();
-                switch (operacion){
-                    case "I":
-                        ItemType itInsert = getItemType();
-                        msq.getItemTypeDao().create(itInsert);
-                        break;
-                    case "A":
-                        List<ItemType> itemTypes = msq.getItemTypeDao().getAll();
-                        for (ItemType a : itemTypes) {
-                            System.out.println(a.toString());
-                        }
-                        break;
-                    case "U":
-                        ItemType itemTypeUpdate = getItemType();
-                        System.out.println("Ingrese el id del tipo de item");
-                        int id = sc.nextInt();
-                        itemTypeUpdate.setId(id);
-                        msq.getItemTypeDao().update(itemTypeUpdate);
-                        break;
-                    case "D":
-                        System.out.println("Ingrese el id del tipo de item que desea eliminar");
-                        ItemType itemtypeD = new ItemType("description");
-                        int idIT = sc.nextInt();
-                        itemtypeD.setId(idIT);
-                        msq.getItemTypeDao().delete(itemtypeD);
-                        break;
-                    case "G":
-                        System.out.println("Ingrese el id del tipo de item que desea listar");
-                        int idi = sc.nextInt();
-                        ItemType oneItemType = msq.getItemTypeDao().get(idi);
-                        System.out.println(oneItemType);
-                        break;
+                itemType();
+                break;
+            case "I":
+                type();
+                break;
+            case "B":
+                bill();
+                break;
+
+
+        }
+    }
+
+    public static void client() throws SQLException, DAOException {
+        Scanner sc = new Scanner(System.in);
+        MySqlManager msq = new MySqlManager("localhost", "root", "1234", "storedb");
+
+        firstInstruction("cliente");
+        String op = sc.next();
+        switch (op) {
+            case "I":
+                Client clientInsert = getClient();
+                msq.getClientDao().create(clientInsert);
+                break;
+            case "A":
+                List<Client> clients = msq.getClientDao().getAll();
+                for (Client a : clients) {
+                    System.out.println(a.toString());
                 }
+                break;
+            case "U":
+                Client clientUpdate = getClient();
+                System.out.println("Ingrese el id del cliente");
+                int id = sc.nextInt();
+                clientUpdate.setClientId(id);
+                msq.getClientDao().update(clientUpdate);
+                break;
+            case "D":
+                System.out.println("Ingrese el id del cliente que desea eliminar");
+                Client cdefault = new Client("d", "d", "d", new Date(0, 0, 0), "d");
+                int idC = sc.nextInt();
+                cdefault.setClientId(idC);
+                msq.getClientDao().delete(cdefault);
+                break;
+            case "G":
+                System.out.println("Ingrese el id del cliente que desea listar");
+                int idG = sc.nextInt();
+                Client oneClient = msq.getClientDao().get(idG);
+                System.out.println(oneClient);
+                break;
+
+        }
+    }
+
+    public static void itemType() throws DAOException, SQLException {
+        Scanner sc = new Scanner(System.in);
+        MySqlManager msq = new MySqlManager("localhost", "root", "1234", "storedb");
+        String op = null;
+        firstInstruction("item");
+        op = sc.next();
+            switch (op){
+                case "I":
+                    ItemType itInsert = getItemType();
+                    msq.getItemTypeDao().create(itInsert);
+                    break;
+                case "A":
+                    List<ItemType> itemTypes = msq.getItemTypeDao().getAll();
+                    for (ItemType a : itemTypes) {
+                        System.out.println(a.toString());
+                    }
+                    break;
+                case "U":
+                    ItemType itemTypeUpdate = getItemType();
+                    System.out.println("Ingrese el id del tipo de item");
+                    int id = sc.nextInt();
+                    itemTypeUpdate.setId(id);
+                    msq.getItemTypeDao().update(itemTypeUpdate);
+                    break;
+                case "D":
+                    System.out.println("Ingrese el id del tipo de item que desea eliminar");
+                    ItemType itemtypeD = new ItemType("description");
+                    int idIT = sc.nextInt();
+                    itemtypeD.setId(idIT);
+                    msq.getItemTypeDao().delete(itemtypeD);
+                    break;
+                case "G":
+                    System.out.println("Ingrese el id del tipo de item que desea listar");
+                    int idi = sc.nextInt();
+                    ItemType oneItemType = msq.getItemTypeDao().get(idi);
+                    System.out.println(oneItemType);
+                    break;
+            }
+    }
+
+    public static void type() throws SQLException, DAOException {
+        Scanner sc = new Scanner(System.in);
+        MySqlManager msq = new MySqlManager("localhost", "root", "1234", "storedb");
+        String op = null;
+        firstInstruction("item");
+        op = sc.next();
+
+        switch (op) {
+            case "I":
+                Item item = getItem();
+                msq.getItemDao().create(item);
+                break;
+            case "A":
+                List<Item> items = msq.getItemDao().getAll();
+                for (Item a : items) {
+                    System.out.println(a.toString());
+                }
+                break;
+            case"U":
+                Item itemUpdate = getItem();
+                System.out.println("Ingrese el id del artículo");
+                int id = sc.nextInt();
+                itemUpdate.setId(id);
+                msq.getItemDao().update(itemUpdate);
+                break;
+            case "D":
+                System.out.println("Ingrese el id del item  que desea eliminar");
+                Item itemD = new Item(1,"d", 0);
+                int idI = sc.nextInt();
+                itemD.setId(idI);
+                msq.getItemDao().delete(itemD);
+                break;
+            case "G":
+                System.out.println("Ingrese el id del item que desea listar");
+                int idi = sc.nextInt();
+                Item oneItem = msq.getItemDao().get(idi);
+                System.out.println(oneItem);
+                break;
+        }
+    }
+
+    public static void bill() throws SQLException,DAOException {
+        Scanner sc = new Scanner(System.in);
+        MySqlManager msq = new MySqlManager("localhost", "root", "1234", "storedb");
+        String op = null;
+        firstInstruction("item");
+        op = sc.next();
+
+        switch (op) {
+            case "I":
+                Bill bill = getBill();
+                msq.getBillDao().create(bill);
+                break;
+            case "A":
+                List<Bill> bills = msq.getBillDao().getAll();
+                for (Bill a : bills) {
+                    System.out.println(a.toString());
+                }
+                break;
+            case"U":
+                Bill billUpdate = getBill();
+                System.out.println("Ingrese el id de la factura");
+                int id = sc.nextInt();
+                billUpdate.setBillId(id);
+                msq.getBillDao().update(billUpdate);
+                break;
+            case "D":
+                System.out.println("Ingrese el id de la factura que desea eliminar");
+                List<Integer> items = new ArrayList<>();
+                items.add(1);
+                Bill billD = new Bill(new Date(0,0,0),1,items);
+                int idI = sc.nextInt();
+                billD.setBillId(idI);
+                msq.getBillDao().delete(billD);
+                break;
+            case "G":
+                System.out.println("Ingrese el id de la factura que desea listar");
+                int idi = sc.nextInt();
+                Bill oneBill = msq.getBillDao().get(idi);
+                System.out.println(oneBill);
+                break;
         }
     }
 
@@ -161,12 +265,49 @@ public class Main {
 
     }
 
+    public static Bill getBill() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese el año de la fecha");
+        int date = sc.nextInt();
+        System.out.println("Ingrese el mes de la fecha en numero");
+        int month = sc.nextInt();
+        System.out.println("Ingrese el dia de la fecha");
+        int day = sc.nextInt();
+        System.out.println("Ingrese id del cliente");
+        int id_client = sc.nextInt();
+        System.out.println("Ingrese el numero de items");
+        int nItems = sc.nextInt();
+        sc.nextLine();
+        List<Integer> items = new ArrayList<>();
+        int d ;
+        for(int i = 1 ; i <= nItems ; i++){
+            System.out.println("Ingrese id del item numero " + i );
+            d  = sc.nextInt();
+            items.add(d);
+        }
+
+        return new Bill(new Date(date),id_client,items);
+    }
+
     public static ItemType getItemType() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Insertar la descripción");
         String description = sc.next();
         return new ItemType(description);
     }
+
+    public static Item getItem(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el tipo de item que es");
+        int type = sc.nextInt();
+        System.out.println("Ingrese la descripcion del item");
+        String description = sc.next();
+        System.out.println("Ingrese el valor del item");
+        int value = sc.nextInt();
+        return new Item(type,description,value);
+    }
+
 
    /* public static void switchOperation(String op){
         switch (op){
